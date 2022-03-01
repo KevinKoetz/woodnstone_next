9479141141ac471434235c232de26eacc8acb463
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-01 14:02:13 +0100

    Add express app and mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,17 @@
 import "dotenv/config"
-console.log("BACKEND: Implement me!");
+import express from "express";
+import mongoose from "mongoose";
+
+if(!process.env.MONGO_DB) throw new Error("env: MONGO_DB not defined.")
+mongoose.connect(process.env.MONGO_DB).then(() => console.log("Connected to DB!")
+).catch(err => {
+    console.log("DB connection Error:", err);
+    process.exit(1)
+})
+
+const PORT = process.env.PORT || 4000
+const app = express();
+
+app.listen(PORT,() => {
+    console.log("App listening on Port:", PORT);
+})
