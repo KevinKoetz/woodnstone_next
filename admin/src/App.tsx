d5564dad5eae612d815b89aa4287324b8b90d534
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-03 09:36:13 +0100

    changed routes to nested routes

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,16 +1,25 @@
 import "./App.css";
-import Layout from "./Components/Layout/Layout"
-import { BrowserRouter as Router } from "react-router-dom";
-
+import Layout from "./Components/Layout/Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    
       <div className="App">
-        <Layout />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<div>Orders</div>} />
+            <Route path="/products" element={<div>Products</div>} />
+            <Route path="/references" element={<div>References</div>} />
+            <Route path="/pages" element={<div> Pages </div>} />
+            <Route path="/users" element={<div> Users </div>} />
+          </Route>
+        </Routes>
       </div>
-    </Router>
+    
   );
 }
+
+
 
 export default App;
