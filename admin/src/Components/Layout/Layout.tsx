2bf6cfd2ef4f951986a1a35da37de0f3c0d848a8
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-04 18:36:52 +0100

    Resolved remaining conversations.

--- admin/src/Components/Layout/Layout.tsx
+++ admin/src/Components/Layout/Layout.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
-import { AppBar, ListItemButton, Button } from "@mui/material";
+import { AppBar} from "@mui/material";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -18,16 +14,7 @@ import { Routes, useNavigate, Route } from "react-router-dom";
 
 const drawerWidth = 240;
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
-export default function LayoutB(props: Props) {
-  const { window } = props;
+export default function Layout() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
@@ -63,9 +50,6 @@ export default function LayoutB(props: Props) {
     </div>
   );
 
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -98,7 +82,7 @@ export default function LayoutB(props: Props) {
       >
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
-          container={container}
+          container={window.document.body}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
