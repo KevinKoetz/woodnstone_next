750a30de588f980b29a7171216bb67bb4105939a
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-07 15:41:17 +0100

    formatted code

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -4,22 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<div>Orders</div>} />
-            <Route path="/products" element={<div>Products</div>} />
-            <Route path="/references" element={<div>References</div>} />
-            <Route path="/pages" element={<div> Pages </div>} />
-            <Route path="/users" element={<div> Users </div>} />
-          </Route>
-        </Routes>
-      </div>
-    
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Orders</div>} />
+          <Route path="/products" element={<div>Products</div>} />
+          <Route path="/references" element={<div>References</div>} />
+          <Route path="/pages" element={<div> Pages </div>} />
+          <Route path="/users" element={<div> Users </div>} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
-
-
 
 export default App;
