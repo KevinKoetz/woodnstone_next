4fcba1f3414778ed3ba39dcf9cd360369c57dcf3
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-07 16:25:33 +0100

    add example route for product

---
+++ backend/src/api/product.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const productRoute = express.Router();
+/* productRoute.post("/", (req, res) => {});
+productRoute.get("/", (req, res) => {});
+productRoute.get("/:id", (req, res) => {});
+productRoute.patch("/:id", (req, res) => {});
+productRoute.delete("/:id", (req, res) => {}); */
+export default productRoute;
