7c1e501642232cac311912c6dca022696693e825
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-08 23:04:14 +0100

    added productRoute

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,9 +1,11 @@
 import {Request, Response, NextFunction} from "express"
 import loginRoute from "./login"
-/* import productRoute from "./product" */
+import productRoute from "./product"
+
+
 
 export default function routes(req: Request, res: Response, next: NextFunction) {
     req.app.use("/login", loginRoute)
-/*     req.app.use("/product", productRoute) */
+    req.app.use("/product", productRoute)
     next()
 }
