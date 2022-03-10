e0be1818a1a9b4d45fa0b841d3cbaf552d86e10e
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-10 16:15:33 +0100

    Co-authored-by: Kevin Kötz <KevinKoetz@users.noreply.github.com>

--- backend/src/api/product.test.ts
+++ backend/src/api/product.test.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" });
+
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
@@ -16,96 +19,137 @@ import Path from "path";
 let mongo: MongoMemoryServer;
 let app: Express;
 let server: Server;
+let address: AddressInfo;
 
 beforeAll(async () => {
-    mongo = await MongoMemoryServer.create(); // creating mongoDB in memory
-    const url = mongo.getUri(); // getting URL of DB
-    await mongoose.connect(url); // connecting mongoose with DB
-    app = express(); // creating express app
-    app.use(passport.initialize());
-    app.use("/product", productRoute); // handle from productRoure middleware
-    server = app.listen(); // start server
+  mongo = await MongoMemoryServer.create(); // creating mongoDB in memory
+  const url = mongo.getUri(); // getting URL of DB
+  await mongoose.connect(url); // connecting mongoose with DB
+  app = express(); // creating express app
+  app.use(passport.initialize());
+  app.use("/product", productRoute); // handle from productRoure middleware
+  server = app.listen(); // start server
+  address = server.address() as AddressInfo;
 });
 
 /* deletes everything in DB to have a clean starting point for each test  */
 beforeEach(async () => {
-    if (mongo) {
-        const collections = mongoose.connection.collections;
+  if (mongo) {
+    const collections = mongoose.connection.collections;
 
-        for (const key in collections) {
-            const collection = collections[key];
-            await collection.deleteMany({});
-        }
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
     }
+  }
 });
 
 afterAll(async () => {
-    if (mongo) {
-        await mongoose.disconnect();
-        await mongo.stop();
-    }
-    server.close();
+  if (mongo) {
+    await mongoose.disconnect();
+    await mongo.stop();
+  }
+  server.close();
 });
 
 describe("the product route should", () => {
-    test("return an array with product on the get request", async () => {
-        const names: string[] = [];
-        for (let i = 0; i < 10; i++) {
-            const name = i.toString();
-            names.push(name);
-            await new Product({
-                description: "Holz",
-                name: name,
-                startingPrice: 100,
-                images: [""],
-                stock: 10,
-                maxOrderAmount: 50,
-            }).save();
-        }
-        const address = server.address() as AddressInfo;
-        if (!address) throw new Error("Server should be running!");
+  test("return an array with product on the get request", async () => {
+    const names: string[] = [];
+    for (let i = 0; i < 10; i++) {
+      const name = i.toString();
+      names.push(name);
+      await new Product({
+        description: "Holz",
+        name: name,
+        startingPrice: 100,
+        images: [""],
+        stock: 10,
+        maxOrderAmount: 50,
+      }).save();
+    }
 
-        const response = await axios.get(
-            "http://localhost:" + address.port + "/product"
-        );
-        expect(response.data).toBeInstanceOf(Array);
+    const response = await axios.get(
+      "http://localhost:" + address.port + "/product"
+    );
+    expect(response.data).toBeInstanceOf(Array);
 
-        names.forEach((name) => {
-            expect(response.data.map((product: any) => product.name)).toContain(name);
-        });
+    names.forEach((name) => {
+      expect(response.data.map((product: any) => product.name)).toContain(name);
     });
-    test("create a new product after post request ", async () => {
-        const address = server.address() as AddressInfo;
-        if (!address) throw new Error("Server should be running!");
+  });
 
-        const name = (Math.random() * 100).toString(); // defining product
-        const product = {
-            description: "Holz",
-            name: name,
-            startingPrice: 100,
-            stock: 10,
-            maxOrderAmount: 50,
-        };
+  test("create a new product after post request ", async () => {
+    const name = (Math.random() * 100).toString(); // defining product
+    const product = {
+      description: "Holz",
+      name: name,
+      startingPrice: 100,
+      stock: 10,
+      maxOrderAmount: 50,
+    };
 
-        const form = new FormData();
-        Object.entries(product).forEach((entry) => form.set(entry[0], entry[1]));
-        form.append("images", new File(["Hello"], "justAName"));
-        form.append("images", new File(["World"], "justAName"));
-       
+    const image1 = new File(
+      Buffer.from(
+        "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAOSURBVChTYxgFJAMGBgABNgABY8OiGAAAAABJRU5ErkJggg==",
+        "base64"
+      ),
+      "image1.png",
+      { type: "image/png" }
+    );
 
-        const encoder = new FormDataEncoder(form);
+    const image2 = new File(
+      Buffer.from(
+        "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYSURBVChTY/z//z8DbsAEpXGAkSnNwAAApeMDEUEua14AAAAASUVORK5CYII=",
+        "base64"
+      ),
+      "image2.png",
+      { type: "image/png" }
+    );
 
-        const response = await axios.post(
-            // sending via post req
-            "http://localhost:" + address.port + "/product",
-            Readable.from(encoder.encode()),
-            { headers: encoder.headers }
-        );
-            throw new Error("implement me!")
-   /*      expect(response.data).toMatchObject(product); //expecting the server to send product back
-        expect(response.data._id).toBeDefined(); // expecting id inside of response data
+    const form = new FormData();
+    Object.entries(product).forEach((entry) => form.set(entry[0], entry[1]));
+    form.append("images", image1);
+    form.append("images", image2);
 
-        const dbProduct = await Product.findById(response.data._id);
-        expect(dbProduct).toMatchObject(product); //expecting to find product in DB */
-    });
+    const encoder = new FormDataEncoder(form);
+
+    try {
+      const response = await axios.post(
+        // sending via post req
+        "http://localhost:" + address.port + "/product",
+        Readable.from(encoder.encode()),
+        { headers: encoder.headers }
+      );
+
+      console.log("here?");  
+      expect(response.data).toMatchObject(product); //expecting the server to send product back
+      expect(response.data._id).toBeDefined(); // expecting id inside of response data
+
+      const dbProduct = await Product.findById(response.data._id);
+      expect(dbProduct).toMatchObject(product); //expecting to find product in DB
+      if (!dbProduct) throw new Error("Product not in DB.");
+      expect(dbProduct.images).toHaveLength(2);
+
+      //Get the image from the URL and expect it to be the same
+      const downloadedImage1 = (
+        await axios.get(dbProduct.images[0], { responseType: "arraybuffer" })
+      ).data;
+      expect(
+        Buffer.from(await image1.arrayBuffer()).equals(
+          Buffer.from(downloadedImage1)
+        )
+      ).toBe(true);
+
+      const downloadedImage2 = (
+        await axios.get(dbProduct.images[1], { responseType: "arraybuffer" })
+      ).data;
+      expect(
+        Buffer.from(await image1.arrayBuffer()).equals(
+          Buffer.from(downloadedImage1)
+        )
+      ).toBe(true);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
--- backend/src/api/product.ts
+++ backend/src/api/product.ts
@@ -1,35 +1,78 @@
+
 import express from "express";
 import Product from "../models/Product";
-import multer from 'multer'
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage, Options } from "multer-storage-cloudinary";
 
-const upload = multer({ dest: "upload/" })
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+
+const storageCloudinary = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "woodnstone",
+    format: async (req: Request, file: any) => {
+      let extension = "";
+      
+      if (file.mimetype.includes("image")) {
+        extension = file.mimetype.split("/")[1];
+
+        if (extension === "jpeg") extension = "jpg";
+      }
+      console.log("extension:", extension)
+      return extension;
+    },
+    public_id: (req, file) =>
+      `${req.body._id}-${Date.now()}-${file.originalname}`,
+  },
+} as Options);
+
+
+const upload = multer({
+  storage: storageCloudinary,
+});
 
 const productRoute = express.Router();
-productRoute.post("/", upload.array("images"), async (req, res) => {
-
-
+productRoute.post(
+  "/",
+  (req, res, next) => {
+    
     try {
-        const product = new Product({ ...req.body, images: (req.files as any)?.map((file: any) => file.filename) })  // creating new instance of class Product 
-        await product.save()                    // has been created and saved after calling save()
-        /* res.setHeader('Content-Type', 'application/json') */
-        /* res.send(JSON.stringify(product)) */
-        res.json(product)
+        upload.array("images")(req,res,next);
+        console.log("Here!!!!");
+        
+      } catch (error) {
+        
+      } 
+  },
+  async (req, res) => {
+    try {
+      const product = new Product({
+        ...req.body,
+        images: (req.files as any)?.map((file: any) => file.filename),
+      }); // creating new instance of class Product
+      await product.save(); // has been created and saved after calling save()
+      /* res.setHeader('Content-Type', 'application/json') */
+      /* res.send(JSON.stringify(product)) */
+      res.json(product);
     } catch (error: any) {
-        res.status(400)
-        res.send(error.message)
+      res.status(400);
+      res.send(error.message);
     }
-});
-
-
-
+  }
+);
 
 productRoute.get("/", async (req, res) => {
-    const products = await Product.find().exec()
-    res.json(products)
+  const products = await Product.find().exec();
+  res.json(products);
 });
 
-productRoute.get("/:id", (req, res) => { });
-productRoute.patch("/:id", (req, res) => { });
-productRoute.delete("/:id", (req, res) => { });
+productRoute.get("/:id", (req, res) => {});
+productRoute.patch("/:id", (req, res) => {});
+productRoute.delete("/:id", (req, res) => {});
 export default productRoute;
-
