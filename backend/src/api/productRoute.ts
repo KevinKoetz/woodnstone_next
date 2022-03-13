a7d9ccddca0fa3f046ee0d7edf5f1c12846e61e8
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-13 16:26:00 +0100

    starting over

--- backend/src/api/productRoute.ts
+++ backend/src/api/productRoute.ts
@@ -82,14 +82,7 @@ productRoute.delete("/:id", async (req, res) => {
 });
 
 productRoute.patch("/:id", async (req, res) => {
-  try {
-    const _id = req.params.id
-    const updateProduct = await Product.findByIdAndUpdate(_id, req.body)
-    res.send(updateProduct)
-  } catch (error) {
-    res.status(404).send(updateProduct)
 
-  }
 
 
 });
