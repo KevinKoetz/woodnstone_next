e82d860cdf74cf8503b7c64e524cc966be97efc1
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-15 16:36:15 +0100

    replaced products <div> with Product component

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import { Provider as UserContextProvider } from "./Components/Auth/Auth";
 import RequireAuth from "./Components/RequireAuth/RequireAuth";
+import Products from "./Components/Products/Products";
 
 function App() {
   
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<RequireAuth><Layout /></RequireAuth>}>
             <Route path="orders" element={<RequireAuth><div>Orders</div></RequireAuth>} />
-            <Route path="products" element={<RequireAuth><div>Products</div></RequireAuth>} />
+            <Route path="products" element={<RequireAuth><Products /></RequireAuth>} />
             <Route path="references" element={<RequireAuth><div>References</div></RequireAuth>} />
             <Route path="pages" element={<RequireAuth><div> Pages </div></RequireAuth>} />
             <Route path="users" element={<RequireAuth><div> Users </div></RequireAuth>} />
