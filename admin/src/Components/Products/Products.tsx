c1b3dbf908e8efd55c144a45a0af204a81501f27
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-15 15:34:11 +0100

    Created Products component and added ProductOverview and PageLayout

---
+++ admin/src/Components/Products/Products.tsx
@@ -0,0 +1,17 @@
+import PageLayout from "../Layout/PageLayout"
+import ProductOverview from "./ProductOverview/ProductOverview"
+
+
+function Products() {
+
+ return (
+     <>
+        <ProductOverview />
+        <PageLayout />
+     </>
+ )
+
+
+} 
+
+export default Products
