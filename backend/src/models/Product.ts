20607470568cd51e06b9ab9ed3dc81ee57643859
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-16 17:02:21 +0100

    Update Product Schema to include information for frontend

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,22 +1,43 @@
-import { Schema, model } from 'mongoose'
-import { Product } from '../../../types'
-
-
+import { Schema, model, Types } from "mongoose";
+import { Product } from "../../../types";
 
 const isInteger = (number: number) => {
-    return number === Math.floor(number)
-}
+  return number === Math.floor(number);
+};
 
 const schema = new Schema<Product>({
-    name: { type: String, required: true, unique: true, },
-    description: { type: String, required: true },
-    startingPrice: { type: Number, required: true, min: 0, validate: isInteger }, //Prices to be stored in euro-cents (no comma values)
-    stock: { type: Number, required: true, min: 0, validate: isInteger },
-    maxOrderAmount: { type: Number, required: true, min: 1, validate: isInteger },
-    images: { type: [String], required: true, minlength: 1 }
-
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    important: true,
+    inputType: "text",
+  },
+  description: { type: String, required: true, inputType: "textarea" },
+  startingPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+    validate: isInteger,
+    important: true,
+    inputType: "number",
+  }, //Prices to be stored in euro-cents (no comma values)
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+    validate: isInteger,
+    important: true,
+    inputType: "number",
+  },
+  maxOrderAmount: {
+    type: Number,
+    required: true,
+    min: 1,
+    validate: isInteger,
+    inputType: "number",
+  },
+  images: { type: [String], required: true, minlength: 1, inputType: "file" },
 });
 
-export default model('Product', schema)
-
-
+export default model("Product", schema);
