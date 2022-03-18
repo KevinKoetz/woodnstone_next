95ca792bfcda47f1614d14be75fa25fa820402d6
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-03-18 15:51:37 +0100

    Fix backgroundSize in Layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -17,8 +17,7 @@ function Layout({ children }: LayoutProps) {
         minHeight:"100vh",
         background:
           "url(https://images.unsplash.com/photo-1604411853851-8b4d5d910545?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)",
-        backgroundSize: "con",
-        
+        backgroundSize: "cover",
       }}
     >
       <Header  pages={pages} />
